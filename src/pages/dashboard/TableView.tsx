7cad4fda4003731a0b061c6a804189ac34ab48d7
TimothyGodwin <92355417+TimothyGodwin@users.noreply.github.com>
2025-02-14 18:45:21 +0530

    Update TableView.tsx

--- src/pages/dashboard/TableView.tsx
+++ src/pages/dashboard/TableView.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { ColumnsType } from 'antd/es/table';
+import React, { useState } from 'react'
 import { Space, Image, Table } from 'antd';
 import Button from '../../components/button/Button';
 import { UserType } from '../../types';
 import '../../styles/TableStyles.css';
+import DeleteModal from '../../components/deleteModal/DeleteModal';
+import showNotification from '../../components/notification/Notification';
+import { useDispatch } from 'react-redux';
+import { setDeleteData, setDeleteDataMulti } from '../../globalStore/slices/IdSlices';
+import { getData } from './helper';
 
 const TableView = ({ data, handleUseAction }) => {
+    const userTrack = getData();
+    const dispatch: any = useDispatch();
+    const [openDeleteModal, setOpenDeleteModal] = useState(false);
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
 
-    const columns: ColumnsType<UserType> = [
+    const onSelectChange = (newSelectedRowKeys) => {
+        setSelectedRowKeys(newSelectedRowKeys);
+    };
+
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
+
+    const columns: any = [
         {
             title: '',
             dataIndex: 'avatar',
@@ -29,16 +46,19 @@ const TableView = ({ data, handleUseAction }) => {
             title: 'Email',
             dataIndex: 'email',
             key: 'email',
+            sorter: (a, b) => { return a.email.localeCompare(b.email) },
             render: (text) => <span className="text-blue-link">{text}</span>,
         },
         {
             title: 'First Name',
             dataIndex: 'first_name',
+            sorter: (a, b) => { return a.first_name.localeCompare(b.first_name) },
             key: 'first_name',
         },
         {
             title: 'Last Name',
             dataIndex: 'last_name',
+            sorter: (a, b) => { return a.last_name.localeCompare(b.last_name) },
             key: 'last_name',
         },
         {
@@ -53,15 +73,38 @@ const TableView = ({ data, handleUseAction }) => {
         },
     ];
 
+    const handleDeleteUser = async () => {
+        dispatch(setDeleteDataMulti(selectedRowKeys));
+        showNotification.SUCCESS('User deleted successfully');
+        const trackActivity = [...userTrack, { id: Date.now(), action: `Multiple User deleted` }];
+        sessionStorage.setItem('trackActivity', JSON.stringify(trackActivity));
+        handleClose();
+        setSelectedRowKeys([])
+    }
+
+    const handleClose = () => {
+        setOpenDeleteModal(false)
+    }
+    const onDelete = (status: boolean) => {
+        if (status) handleDeleteUser();
+        else handleClose()
+    }
+
     return (
         <div className="w-full overflow-x-auto">
+            {selectedRowKeys?.length > 0 &&
+                <Button type="delete" onClick={() => {
+                    setOpenDeleteModal(true)
+                }}>Delete</Button>}
             <Table
+                rowSelection={rowSelection}
                 columns={columns}
                 dataSource={data}
                 pagination={false}
                 bordered={false}
                 className="custom-table"
             />
+            {openDeleteModal && <DeleteModal open={openDeleteModal} onDelete={onDelete} />}
         </div>
     )
 }
