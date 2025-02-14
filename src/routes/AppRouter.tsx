a32c8d03e73415514675337d8a64101070242a90
TimothyGodwin <92355417+TimothyGodwin@users.noreply.github.com>
2025-02-14 18:42:33 +0530

    Update AppRouter.tsx

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import Login from "../pages/Login/Login"
 import Dashboard from "../pages/dashboard/Dashboard"
 import ProtectedRoute from "./ProtectedRoute"
+import HistoryData from "../pages/dashboard/HistoryData"
 
 const AppRouter = () => {
     return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
             <Route path="/login" element={<Login />} />
             <Route element={<ProtectedRoute />}>
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/history" element={<HistoryData />} />
             </Route>
         </Routes>
     )
